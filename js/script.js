0e489306c9db3fec7a864b3fa26d61f825280a19
Vaseeyy <125756937+Vaseeyy@users.noreply.github.com>
2023-03-28 19:38:06 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-const body = document.querySelector("body");
-
-const modeToggle = document.getElementById('mode-toggle');
-
-const modeStatus = document.querySelector('.mode-status');
-
 function toggleMode() {
    body.classlist.toggle('light-mode');
 
