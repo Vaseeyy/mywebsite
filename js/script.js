9e336696c5df7f32b77259070156c23e1b30b32f
Vaseeyy <125756937+Vaseeyy@users.noreply.github.com>
2023-03-28 19:41:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,2 @@
 function toggleMode() {
    body.classlist.toggle('light-mode');
-
-const modeMessage = body.classList.contains('light-mode')
-   'Dark-Mode'
-   : "Light-Mode"
-   
-modeStatus.innerText = "Currently in " + modeMessage;
-}
-
-modeToggle.addEventListener('click', toggleMode);
