6cb878d06fd847f1a210e4bb901f572689193870
Vaseeyy <karladamvasey@gmail.com>
2023-04-18 19:29:49 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-$( ".change" ).on("click", function() {
-            if( $( "body" ).hasClass( "dark" )) {
-                $( "body" ).removeClass( "dark" );
-                $( ".change" ).text( "OFF" );
-            } else {
-                $( "body" ).addClass( "dark" );
-                $( ".change" ).text( "ON" );
-            }
-        });
 
 function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark");
+    var element = document.body;
+    element.classList.toggle("dark");
 }
 alert("Welcome to My Website, please enjoy your stay!");
